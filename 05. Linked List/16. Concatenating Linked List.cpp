62bd414f9c2fdc6c9bd47a492dc2e8cab842ca2f
arjundubey-cr <arjundubeycr@gmail.com>
2020-06-19 18:27:29 +0530

    commit

--- 05. Linked List/16. Concatenating Linked List.cpp
+++ 05. Linked List/16. Concatenating Linked List.cpp
@@ -4,7 +4,7 @@ struct Node
 {
 	int data;
 	struct Node* next;
-}*first=NULL;
+}*first=NULL, *second=NULL;
 
 void create(int A[], int n)
 {
@@ -24,6 +24,25 @@ void create(int A[], int n)
 	}
 }
 
+
+void create2(int B[], int n)
+{
+	struct Node *temp, *last;
+	second = (struct Node*)malloc(sizeof(struct Node));
+	second->data=B[0];
+	second->next=NULL;
+	last=second;
+	
+	for(int i=1;i<n;i++)
+	{
+		temp=(struct Node*)malloc(sizeof(struct Node));
+		temp->data=B[i];
+		temp->next=NULL;
+		last->next=temp;
+		last=temp;
+	}
+}
+
 void Display(struct Node* p){
 	while(p)
 	{
@@ -34,17 +53,23 @@ void Display(struct Node* p){
 
 void concatenation(struct Node* p, struct Node *q)
 {
-	while(p)
-		p=p->next;
 	
+	while(p->next!= NULL)
+	{
+		p=p->next;
+	}
 	p->next=q;
 	q=NULL;
+	
 }
 
 int main()
 {	
 	int A[] = {3,5,5,6,8,8};
+	int B[] = {3,4,4,6,7,8};
 	create(A, 6);
+	create2(B, 6);
+	concatenation(first, second);
 	Display(first);
 	printf("\n");
 }
--- 05. Linked List/17.Merging two Linked Lists.cpp
+++ 05. Linked List/17.Merging two Linked Lists.cpp
@@ -4,7 +4,7 @@ struct Node
 {
 	int data;
 	struct Node* next;
-}*first=NULL;
+}*first=NULL, *second=NULL, *third=NULL;
 
 void create(int A[], int n)
 {
@@ -24,6 +24,25 @@ void create(int A[], int n)
 	}
 }
 
+void create2(int B[], int n)
+{
+	struct Node *temp, *last;
+	second = (struct Node*)malloc(sizeof(struct Node));
+	second->data=B[0];
+	second->next=NULL;
+	last=second;
+	
+	for(int i=1;i<n;i++)
+	{
+		temp=(struct Node*)malloc(sizeof(struct Node));
+		temp->data=B[i];
+		temp->next=NULL;
+		last->next=temp;
+		last=temp;
+	}
+}
+
+
 void Display(struct Node* p){
 	while(p)
 	{
@@ -32,19 +51,48 @@ void Display(struct Node* p){
 	}
 }
 
-void concatenation(struct Node* p, struct Node *q)
-{
-	while(p)
-		p=p->next;
+void Merge(struct Node* first, struct Node* second){
+	struct Node *last;
+	if(first->data < second->data){
+		third=last=first;
+		first=first->next;
+		last->next=NULL;
+	}
+	else{
+		third=last=second;
+		second=second->next;
+		third->next=NULL;
+	}
+	while(first&&second)
+	{
+		if(first->data<second->data)
+		{
+			last->next=first;
+			last=first;
+			first=first->next;
+			last->next=NULL;
+		}
+		else{
+			last->next=second;
+			last=second;
+			second=second->next;
+			last->next=NULL;
+		}
+	}
 	
-	p->next=q;
-	q=NULL;
+	
+	first!=NULL?last->next=first:last->next=second;
 }
 
 int main()
 {	
 	int A[] = {3,5,5,6,8,8};
+	int B[] = {2,3,4,5,6,7};
+	
 	create(A, 6);
-	Display(first);
+	create2(B, 6);
+	
+	Merge(first, second);
+	Display(third);
 	printf("\n");
 }
