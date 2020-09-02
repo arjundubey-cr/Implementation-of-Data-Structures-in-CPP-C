f4595263bf623ac314146dc55d9561a37a52031c
arjundubey-cr <arjundubeycr@gmail.com>
2020-09-03 00:55:56 +0530

    stack

--- 06. Stack/01.Creating Stack using Array.cpp
+++ 06. Stack/01.Creating Stack using Array.cpp
@@ -1,4 +1,10 @@
+//Stack 
+//Linear Data Structure based on LIFO
+//Basic Operations: create, push, pop, isEmpty, isFull etc.
+//Following is array implementation of stack.
+
 #include <iostream>
+#include <stdio.h>
 using namespace std;
 
 struct Stack{
@@ -6,71 +12,63 @@ struct Stack{
 	int top;
 	int *S;
 };
-
 void create(struct Stack *st)
 {
-	printf("Enter Size:- ");
-	scanf("%d",&(st->size));
-	st->top=-1;
-	st->S=(int *)malloc(st->size*sizeof(int));
-	printf("Stack Created");
+	printf("Enter size of stack: ");
+	scanf("%d", &(st->size));
+	st->top=0;
+	
+	st->S = (int *)malloc((st->size)*sizeof(int));
+	printf("Stack is created");	
 }
-void Display(struct Stack st)
+void display(struct Stack *st)
 {
 	int i;
-	for(i=st.top;i>=0;i--)
-		printf("%d", st.S[i]);
+	for(i=st->top;i>=0;i--)
+		printf("%d | ", st->S[i]);
 	printf("\n");
 }
-void push(struct Stack *st, int x)
+void push(struct Stack *st, int data)
 {
 	if(st->top==st->size-1)
-	{
-		printf("StackOverflow\n");
-	}
-	else
-	{
-		st->top++;
-		st->S[st->top]=x;
-	}
-}
-int pop(struct Stack *st)
-{
-	int x=-1;
-	
-	if(st->top==-1)
-		printf("Stack Underflow\n");
+		printf("Stack Overflow \n");
 	else{
-		x=st->S[st->top--];
+		st->top+=1;
+		st->S[st->top]=data;
+		printf("Data is pushed to stack\n");
 	}
-	return x;
 }
-int peek(struct Stack st, int index)
+void pop(struct Stack *st)
 {
-	int x=-1;
-	if(st.top-index+1<0)
-		printf("Invalid Index\n");
-	x=st.S[st.top-index+1];
-	return x;
+	if(st->top==-1){
+		cout<<"Stack is Empty\n";
+		return;	
+	}
+	cout<<st->S[st->top];
+	printf("Element popped up. \nNew top is %d",st->S[--(st->top)]);
 }
+bool isEmpty(struct Stack *st)
+{
+	if(st->top < (st->size - 1))
+		return true;
+	return false;
+} 
 
-int isEmpty(struct Stack st)
+bool isFull(struct Stack *st)
 {
-	if(st.top==-1)
-		return 1;
-	return 0;
-}
-int isFull(struct Stack st)
-{
-	return st.top==st.size-1;	
-}
-int stackTop(struct Stack st)
-{
-	if(!isEmpty(st)) return st.S[st.top];
-	return -1;
+	if(st.Top == (st->size-1))
+		return true;
+	else 
+		return false;
 }
 int main()
 {
 	struct Stack st;
 	create(&st);
+	push(&st, 14);
+	push(&st, 11);
+	push(&st, 5);
+	push(&st, 10);
+	display(&st);
+	pop(&st);
 }
