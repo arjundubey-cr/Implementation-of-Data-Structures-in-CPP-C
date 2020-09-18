61002760a610ecf0803e8931064d647236f65425
arjundubey-cr <arjundubeycr@gmail.com>
2020-09-19 00:04:49 +0530

    stack

---
+++ 06. Stack/04.Paranthesis_Matching.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node *next;
+} *top;
+class Stack{
+    private:
+        Node *next;
+    public:
+        void push();
+        
+};
+int main()
+{
+
+}
